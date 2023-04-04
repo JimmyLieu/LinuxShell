3d394679411b459076929c44f890548432036a0a
Jimmy Lieu <98052856+JimmyLieu@users.noreply.github.com>
2023-04-03 21:30:08 -0500

    Update JimmyLieuProject3.java

--- JimmyLieuProject3.java
+++ JimmyLieuProject3.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class JimmyLieuProject3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException, InterruptedException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input;
         while (true) {
@@ -25,13 +25,21 @@ public class JimmyLieuProject3 {
             switch (input.toLowerCase()) {
             //load directory
                 case "ls":
-                    executeCommand("cmd /c dir");
+                    executeCommand("cmd /c dir \n");
+                   
                     break;
                 case "pwd":
                     executeCommand("cmd /c cd");
+                    System.out.println();
                     break;
                 case "clear":
-                    executeCommand("cmd /c cls");
+                try{
+                    if (System.getProperty("os.name").contains("Windows"))
+                        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                    else 
+                        Runtime.getRuntime().exec("clear");
+                } catch (IOException | InterruptedException ex) {}
+                    
                     break;
                 case "date":
                     // Get the current date and time
@@ -56,27 +64,45 @@ public class JimmyLieuProject3 {
                     executeCommand("cmd /c echo %username%");
                     break;
                 default:
+                if (input.startsWith("help ")){ //check if input starts with "help "
+                    String cmd = input.substring(5); // extract the command after "help "
+                    displayCommandHelp(cmd); //display the help for the specified command
+                }else{
                     System.out.println("Unknown command!");
+                }
             }
 
         }
     }
 
-    private static String executeCommand(String command) {
+    private static void executeCommand(String command) {
         try {
             Process process = Runtime.getRuntime().exec(command);
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.printf(line);
+            if (command.toLowerCase().equals("dir")) {
+                System.out.println("Name\t\t\tSize\tLast Modified"); // Print header
+                System.out.println("--------------------------------------------------"); // Print separator
+                while ((line = reader.readLine()) != null) {
+                    String[] tokens = line.split("\\s+"); // Split line by whitespace
+                    if (tokens.length >= 4) { // Check if line contains file info
+                        String name = tokens[tokens.length - 1];
+                        String size = tokens[tokens.length - 5];
+                        String lastModified = tokens[tokens.length - 4] + " " + tokens[tokens.length - 3] + " " + tokens[tokens.length - 2];
+                        System.out.printf("%-20s %-10s %-20s\n", name, size, lastModified); // Print formatted output
+                    }
+                }
+            } else {
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return command;
     }
-
+    
     private static void displayHelp() {
         System.out.println("MyShell, version 2.0, runs on Windows 10, developed by Jimmy Lieu");
         System.out.println("Release date March 24, 2023");
@@ -102,4 +128,33 @@ public class JimmyLieuProject3 {
         System.out.printf("%-15s %-15s\n", "whoami", "Displays the name of the programmer");
 
     }
+    private static void displayCommandHelp(String cmd) {
+        switch(cmd.toLowerCase()) {
+            case "ls":
+                System.out.println("Lists all files and directories in the current directory.");
+                break;
+            case "pwd":
+                System.out.println("Displays the current directory.");
+                break;
+            case "clear":
+                System.out.println("Clears the console.");
+                break;
+            case "date":
+                System.out.println("Displays the current day, time, and time zone.");
+                break;
+            case "help":
+                System.out.println("Get help for command cmd ('cmd' is optional). Without cmd, displays a list of available commands.");
+                break;
+            case "exit":
+                System.out.println("Quit console.");
+                break;
+            case "whoami":
+                System.out.println("Displays the name of the programmer.");
+                break;
+            default:
+                System.out.println("Unknown command. Type 'help' for a list of available commands.");
+                break;
+        }
+    }
+    
 }
